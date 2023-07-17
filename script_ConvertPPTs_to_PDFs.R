cc195f77fa25f3e0930a935ec4f1e219b3309478
Catherine (Cat) Sun <37591723+catherinecsun@users.noreply.github.com>
2023-07-17 09:57:06 +0200

    Update script_ConvertPPTs_to_PDFs.R
    added stringr library, cleaned code to ignore the ppts that the function cant handle, added annotation.

--- script_ConvertPPTs_to_PDFs.R
+++ script_ConvertPPTs_to_PDFs.R
@@ -3,10 +3,10 @@
 
 #### load libraries #### 
 # (installing if necessary)
-list.of.packages <- list("dplyr","purrr","docxtractr")
+list.of.packages <- list("dplyr","purrr","docxtractr","stringr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
-do.call("require", list(list.of.packages)) 
+lapply(list.of.packages, require, character.only = TRUE)
 
 # You have to show the way to the LibreOffice 
 set_libreoffice_path("C:/Program Files/LibreOffice/program/soffice.exe")
@@ -15,21 +15,30 @@ set_libreoffice_path("C:/Program Files/LibreOffice/program/soffice.exe")
 # They should all be in the same folder.
 # If they are not, move/or copy them into a single folder.
 #  You can have sub-folders and other non-PPT files in this folder
-loc_of_PPTs<-"C:/Users/cathe/Documents/ZCP/Manuals, Protocols, Etc/Identifications/ID Kits/ZCP ID Kits 21Jun22/Wild Dog/ID KITS"
+loc_of_PPTs<-"D:/Kafue 2023/ID Kits/ZCP ID Kits 11Jul23/Lion"
 
 # Point this script to that location, by setting the working directory
 setwd(loc_of_PPTs)
 
-### list the ppts that are in the folder ####
-ppts_toConvert<-list.files(recursive=FALSE,full.names = TRUE)
+### list the files that are in the folder ####
+files_inFolder<-list.files(recursive=FALSE,full.names = TRUE)
+files_inFolder
 
-# may need to exclude temporary ~$ files too
-toExclude<-c(which(grepl(".ppt", ppts_toConvert, fixed = TRUE)==FALSE),
-             which(grepl("./~$", ppts_toConvert, fixed = TRUE)))
+# exclude temporary ~$ files 
+toExclude<-c(which(grepl("./~$", files_inFolder, fixed = TRUE)))
+ppts_toConvert<-files_inFolder[-toExclude]
 
-ppts_toConvert<-ppts_toConvert[-toExclude]
+#and only include the pptx's because that's all docxtractr::convert_to_pdf can use
+toInclude<-which(grepl(".pptx", ppts_toConvert, fixed = TRUE))
+ppts_toConvert<-ppts_toConvert[toInclude]
+ppts_toConvert
 
 ### this runs through each PPT and creates a PDF version 
-##### uses the same name and puts it in the same folder
+##### uses the same name (but pdf) and puts it in the same folder
 new.names<-str_replace(ppts_toConvert[1:2],".pptx",".pdf")
-sapply(ppts_toConvert[1:2],convert_to_pdf,new.names)
+
+#this is what you'll be getting 
+new.names
+
+#so do it
+sapply(ppts_toConvert,convert_to_pdf,new.names)
